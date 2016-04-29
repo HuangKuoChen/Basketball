41e02b7bcdf56cd9222d17b15ce0ba22c9bf9af4
HuangKuoChen <jimcorn615@gmail.com>
2016-04-29 23:01:17 +0900

    Basketball

---
+++ Assets/background.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class background : MonoBehaviour {
+
+    public GameObject red, blue;
+    public GameObject ballObj;
+    public GameObject overText;
+    public GameObject startBtn, exitBtn;
+    public Text redScoreText, blueScoreText;
+    public int redScore, blueScore;
+    public bool play;
+    public static background Instance;
+
+    public void AddScore(string which)
+    {
+        switch (which)
+        {
+            case "red":
+                redScore++;
+                redScoreText.text = "Score: " + redScore;
+                Debug.Log("Red scored" + redScore);
+                if (redScore == 10)
+                    gameOver("Red");
+                break;
+            case "blue":
+                blueScore++;
+                blueScoreText.text = "Score: " + blueScore;
+                Debug.Log("Blue scored" + blueScore);
+                if (blueScore == 10)
+                    gameOver("Blue");
+                break;
+        }
+    }
+
+    public void gameStart()
+    {
+        play = true;
+        startBtn.SetActive(false);
+        exitBtn.SetActive(false);
+        overText.SetActive(false);
+        Reset();
+        Instantiate(ballObj, new Vector3(0, 0, 0), gameObject.transform.rotation);
+    }
+
+    public void gameOver(string which)
+    {
+        play = false;
+        overText.SetActive(true);
+        overText.GetComponent<Text>().text = which + "  Wins";
+        startBtn.SetActive(true);
+        exitBtn.SetActive(true);
+        ball.Instance.gameOver();
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void Reset()
+    {
+        overText.SetActive(false);
+        redScore = 0;
+        redScoreText.text = "Score : " + redScore;
+        blueScore = 0;
+        blueScoreText.text = "Score : " + blueScore;
+    }
+
+    // Use this for initialization
+    void Start () {
+        startBtn.SetActive(true);
+        exitBtn.SetActive(true);
+        Instance = this;
+        Input.multiTouchEnabled = true;
+        play = false;
+        Reset();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //if (Input.GetKey("a"))
+        //    ball.Instance.gameOver();
+
+        // If screen was touched.
+        if (Input.touchCount > 0 && play)
+        {
+            // Detect which object was touched.
+            foreach (Touch touch in Input.touches)
+            {
+                Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(touch.position).x, Camera.main.ScreenToWorldPoint(touch.position).y);
+                RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+                if (hit.transform.gameObject.tag == "redUp" && red.transform.position.y < 3)
+                {
+                    red.transform.position += new Vector3(0, 0.1f, 0);
+                    Debug.Log("redUp clicked");
+                }
+                else if (hit.transform.gameObject.tag == "redDown" && red.transform.position.y > -3)
+                {
+                    red.transform.position -= new Vector3(0, 0.1f, 0);
+                    Debug.Log("redDown clicked");
+                }
+                else if (hit.transform.gameObject.tag == "blueUp" && blue.transform.position.y < 3)
+                {
+                    blue.transform.position += new Vector3(0, 0.1f, 0);
+                    Debug.Log("blueUp clicked");
+                }
+                else if (hit.transform.gameObject.tag == "blueDown" && blue.transform.position.y > -3)
+                {
+                    blue.transform.position -= new Vector3(0, 0.1f, 0);
+                    Debug.Log("blueDown clicked");
+                }
+            }
+            
+        }
+    }
+}
---
+++ Assets/ball.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+
+    public float ballXSpeed, ballYSpeed;
+    public float xface, yface;
+    private float time;
+    public static ball Instance;
+
+    public void gameOver()
+    {
+        Destroy(gameObject);
+    } 
+
+    void Reset(float x)
+    {
+        xface = x;
+        yface = 0.1f;
+        ballXSpeed = 1;
+        ballYSpeed = 1;
+        time = 0;
+        gameObject.transform.position = new Vector3(0, 0, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "player")
+        {
+            xface *= -1;
+            Vector2 dir = new Vector2(gameObject.transform.position.x - col.transform.position.x, gameObject.transform.position.y - col.transform.position.y);
+            dir.Normalize();
+            ballYSpeed = dir.y * -2;
+            //Debug.Log("collisin player");
+        } else if (col.tag == "upside" || col.tag == "downside")
+        {
+            yface *= -1;
+            //Debug.Log("collisin side");
+        } else if(col.tag == "redside")         // If ball hits redside, reset and ball move to blueside
+        {
+            background.Instance.AddScore("blue");
+            Reset(0.1f);
+        } else if(col.tag == "blueside")        // If ball hits blueside, reset and ball move to redside
+        {
+            background.Instance.AddScore("red");
+            Reset(-0.1f);
+        }
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Instance = this;
+        Reset(0.1f);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        time += Time.deltaTime;
+        gameObject.transform.position += new Vector3(ballXSpeed * xface * (1 + time / 10), ballYSpeed * yface * ( 1 + time / 10), 0);
+        gameObject.transform.Rotate(0, 0, 360 * Time.deltaTime);
+    }
+}
